0da83a1c26d8c400625d83dbf7733880d70dd366
sathish19961412 <sathishfreelanc5@gmail.com>
2023-03-25 12:43:29 +0530

    add to index1.js

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app =express()
 const bodyParser = require('body-parser');
 const multer =require('multer');
 const path = require('path')
+
+
+const cors = require('cors');
 app.use(express.static('./public'));
 app.use(bodyParser.json());
+app.use(cors());
 
 //Use of Multer Package
 
 let storage=multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,'./public/images')
+        cb('./public/images')
     },
     filename:(req,file,cb)=>{
        cb(nll,file.filename+'_'+Date.now() + path.extname(file.originalname))
@@ -44,7 +48,7 @@ app.post('/upload',(req,res)=>{
       })
 })
 
-const PORT=process.env.PORT || 1996;
+const PORT=process.env.PORT || 8000;
 app.listen(PORT,()=>{
     console.log(`server is running at Port ${PORT}`)
 })
